851ac02f3b7c30ca01ccd6be563cd79c39c4d0a5
Amin Mastani <amin.mastani@yahoo.com>
2017-04-21 16:29:56 +0430

    Change db config

--- Config.php
+++ Config.php
@@ -4,7 +4,7 @@ defined('ACCESS') or die(header('HTTP/1.1 403 Forbidden'));
 const DEFAULT_PATH = '';
 
 const DB_SERVER   = 'localhost';
-const DB_NAME       = 'cheraghr_minoo96mvc';
-const DB_USERNAME = 'cheraghr_mvc';
-const DB_PASSWORD = '9LD]7k$g]t*v';
+const DB_NAME       = 'minoo96';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = '';
 const DB_PORT     = '3306';
